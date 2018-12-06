92ccab7c400fbf9c8bcfc7bcc90aae3ab83e1e65
Tang Sing Lun <cokelight@gmail.com>
2018-12-06 21:36:22 +0800

    frogger arcade initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Effective Javascript Frogger Arcade
+
+## Table of Contents
+
+* [Installation](#Installation)
+* [Instructions](#instructions)
+* [Level Customization](#customization)
+* [Project Reference Sources Or Links](#references)
+
+
+## Installation
+
+The game runs on any browser or mobile devices. You can just click on the `index.html` file and play. If you choose to install it on the web server. You can
+create a folder (for example `Game`) and copy the folder `css` , `images` , `js` and the file `index.html` and paste it inside the `Game` folder. Copy the `Game` folder to the root directory of the web server. Inorder to access the web site for example `localhost`: you just need to type in `http://localhost/Game/index.html`.
+
+
+## Instructions
+
+In this game you have a Player and Enemies (bugs). The goal of the player is to reach the water, without colliding into any one of the enemies.
+
+To get started, clck the `Play` button. The timer will start automatically. To `Restart` click the clockwise `arrow` button.
+
+* Move the player left, right, up and down with the arrows keys on the
+  keyboard.
+* Once a the player collides with an enemy, the game is reset and the player    moves back to the starting square.
+* Once the player reaches the water (i.e., the top of the game board), the      game is won. A modal will be showing out and display the summary. If you
+  want to continue click the "Restart" button.
+* You have three options to set the levels of difficulty of the game.           "Easy", "Medium" and "Hard". Each levels is base on the speed of the bugs,    the no of      the bugs. Try It Out! 
+* To customize each levels base on your choice. You can customize the           variables in the coding. NOTE: This is only for advance user with the         knowledge of   javascript programming.
+
+## Customization
+
+For this part, this is only for advance user with the knowledge of javascript programming. We will customize the "Easy" Level for demostration. Each Level applies the same.
+
+The only file you need to modify is "**app.js**". The function you need to modify
+is **gameSetUp()**.
+
+Below lists the variables and their use.
+
+* noOfBugs - This is the number of Bug(s) you want for the game. For example:   for the "Easy" level. I want the total number of Bug(s) is 4. That means      the  game will dispatch 4 Bugs at the same time.
+* treshold - The treshold variable is a number that multiplies to the distance     of the bug (which is -101 off the screen) at the beginning. You can think of the distance     between each bug at    the beginning time. As the treshold number increases the   distance between     each bugs increases. Lets modify the treshold number     to 5.
+* speedMax - This is the maximun speed of the bug crossing through. As the number increases the speed also increases.
+* speedMin - This is the minimun speed of the bug crossing through. Why we need the minimun speed? This is because a random number will be generated for the bug between the maximun speed and the minimun speed. Therefore each bug will have a random speed that base on the maximun and minimun value. Some bugs will move faster and some bugs will move slower (each is base on the random number generated). The interesting part is as the differences of the maximun and the minimun speed shorten, the speed of all bugs maintain a stable speed (either fast or slow). As the difference is largely further  apart, the speed of each bugs will be moving either very fast or very slow. For the "Easy" level, lets set the maximun value to 600 and the minimun value to 400.
+* yaxes - This is an array. This **MUST BE CORRESPONT** to the "noOfBugs". For the "Easy" level we set the noOfBugs to 4, therefore the "yaxes" array SHOULD HAVE the total of 4 Items. The Items must be contain the following values:
+    * firstRow
+    * secondRow
+    * thirdRow
+
+    As the name describe the value will be the bugs position. For example if there are 4 bugs you want to set the number of bug(s) for the first row is 2. For the second row you want it to be 1, and the third row  is 1. Then the array will be:
+
+    `yaxes = [firstRow, secondRow, thirdRow, firstRow];`
+
+    There is no order preferences, you can order the item anyway you like.
+
+## References
+
+* fonts or incons
+    * [Font Awesome](https://fontawesome.com/?from=io)
+* Code References
+    * [CSS Tricks](https://css-tricks.com)
+    * [Stack OverFlow](https://stackoverflow.com/)
+    * [w3schools](https://www.w3schools.com)
+    * [Udacity](https://www.udacity.com/)
+    * [GitHub](https://github.com/)
+    * [Collision Detection](https://stackoverflow.com/questions/13916966/adding-collision-detection-to-images-drawn-on-canvas)
+    
---
+++ js/app.js
@@ -0,0 +1,301 @@
+//We don't use the todate's date, however we create a past date,
+//which allows to set the time (Minutes and Seconds) to zeors.
+let startDate = new Date('2014-01-01 00:00:00');
+
+//these sets the timer, gameStarted and levels to their initial value
+let timerFunction;
+let gameStarted = false;
+let levels = "";
+
+//This is the "Timer" which display the time of every one second.
+function startTimer() {
+
+    //Add one seconds to the Date.
+    startDate.setSeconds(startDate.getSeconds() + 1);
+
+    secTime = Math.floor(startDate / 1000) % 60;
+    minTime = startDate.getMinutes();
+
+    //Display the time
+    document.querySelector("#startTimer").innerHTML = "Time " + (minTime < 10 ? "0" + minTime : minTime) + ":" + (secTime < 10 ? "0" + secTime : secTime);
+}
+
+//Stop the timer , this occurs when restart is clicked or when we finish the game.
+function stopTimer() {
+    //Reset the date to the begin Date.
+    startDate = new Date('2014-01-01 00:00:00');
+    clearTimeout(timerFunction);
+}
+
+// Enemies our player must avoid
+let Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.startPos = 101;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+    if (!gameStarted) {return;}
+
+    if (this.x < this.startPos * 5) {
+        this.x += this.speed * dt;
+    }
+    else {
+        this.x = this.startPos * -1;
+    }
+    
+    //console.log(this.x);
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+let Player = function() {
+    this.centerYdiff = 15;
+    this.moveX = 101;
+    this.moveY = 83;
+    this.x = 2 * this.moveX;
+    this.y = 5 * this.moveY;
+    this.keyTyped = '';
+    this.sprite = 'images/char-boy.png';
+}
+
+// Update the player's position, required method for game
+// These Player "update" funtion is used for collision detection. 
+Player.prototype.update = function() {
+    for (enemy of allEnemies){        
+        if (this.y === enemy.y && (enemy.x + enemy.startPos/2 > this.x && enemy.x < this.x  + this.moveX/2)) {
+               resetGame();                
+        }
+    }
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);   
+};
+
+//This "handleInput" function is used to detect the "UP, DOWN, LEFT ,RIGHT" keys and it is 
+//used to contorl the Player movement. This method is used the Proxy mechanism to detect
+//the keyboard keys. 
+Player.prototype.handleInput = function(keyTyped) {
+
+    const handler = {
+        set(target, prop, value, receiver) {
+
+          if (!gameStarted) {return false;}  
+          
+          if (value === 'up') {                                      
+              if ((target['y'] + target['centerYdiff']) >= target['moveY']) {                  
+                    target['y'] -= target['moveY'];
+              }
+              
+              if ((target['y'] + target['centerYdiff']) < target['moveY']) { 
+                winModal();
+              }              
+          }
+          else if (value === 'down') {
+                if ((target['y'] + target['moveY']) <= (target['moveY'] * 5) - target['centerYdiff']) {
+                        target['y'] += target['moveY'];
+                }
+          }
+          else if (value === 'left') {
+            if (target['x'] >= target['moveX']) {
+                target['x']  -= target['moveX'];
+            }
+          }
+          else if (value === 'right') {
+            if (target['x'] < target['moveX'] * 4) {
+                target['x']  += target['moveX'];
+            }
+          }          
+          return true;       
+        }
+      };
+
+    const proxy = new Proxy(this, handler);
+    proxy.keyTyped = keyTyped;          
+}
+
+//This is the starting function of the Game.
+function startGame() {
+    //Call the timer function which jumps at every one second.
+    timerFunction = setInterval(startTimer, 1000);
+    gameSetUp();
+    document.querySelector('#start').remove();
+    document.querySelector('#article-start').innerHTML = '<i id="start" class="fas fa-play game-start"></i><label for="start"> Start</label>';
+    document.querySelector('#restart').remove();
+    document.querySelector('#article-icons-restart').innerHTML = '<i id="restart" onclick="restartGame();" class="fas fa-redo replay"></i><label for="restart"> Restart</label>';    
+
+    //This variable defines when the game is "pause" or "start".
+    gameStarted = true;
+}
+
+//This function restart the Game, which initailsize all the variable and options to their initial values.
+function restartGame() {
+    stopTimer()
+    resetGame();
+    document.querySelector("#startTimer").innerHTML = "Time 00:00";
+    document.querySelector('#start').remove();
+    document.querySelector('#article-start').innerHTML = '<i id="start" onclick="startGame();" class="fas fa-play game-not-start"></i><label for="start"> Start</label>';
+    document.querySelector('#restart').remove();
+    document.querySelector('#article-icons-restart').innerHTML = '<i id="restart" class="fas fa-redo replay"></i><label for="restart"> Restart</label>';
+    gameStarted = false;
+}
+
+//This function stop the timer.
+function stopTimer() {
+    //Reset the date to the begin Date.
+    startDate = new Date('2014-01-01 00:00:00');
+    clearTimeout(timerFunction);
+}
+
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+let allEnemies = [];
+
+// Place the player object in a variable called player
+const player = new Player();
+
+//This fuctions setup the game. All the options speed, no of bugs, threshold , axes (x,y) are defined here.
+//For more detail instruction please refer the README.md File.
+function gameSetUp() {
+
+    let noOfBugs;
+    let firstRow = 83;
+    let secondRow = firstRow * 2;
+    let thirdRow = firstRow * 3;
+    let yaxes;
+    let treshold;
+    let speedMax = 400;
+    let speedMin = 100;
+    allEnemies = [];
+
+    player.moveX = 101;
+    player.moveY = 83;
+    player.x = 2 * player.moveX;
+    player.y = (5 * player.moveY) - player.centerYdiff;
+
+    // ************************************************************
+    // *   Please note this part is where you can modify the
+    // *   variables for each level.
+    // *
+    // *   For detail instruction on how to modify. Please refer
+    // *   to the README.md file.
+    // ************************************************************
+
+    if (document.querySelector("#easy").checked) {
+        levels = "Easy";
+        noOfBugs = 4;
+        treshold = 4;
+        speedMax = 600;
+        speedMin = 400;
+        yaxes = [firstRow, firstRow, thirdRow, secondRow];
+    }
+    else if(document.querySelector("#medium").checked){
+        levels = "Medium";
+        noOfBugs = 6;
+        treshold = 6;
+        speedMax = 500;
+        speedMin = 450;
+        yaxes = [firstRow, secondRow, thirdRow, firstRow, firstRow, thirdRow];
+    }
+    else if(document.querySelector("#hard").checked){
+        levels = "Hard";
+        noOfBugs = 8;
+        treshold = 6;
+        speedMax = 550;
+        speedMin = 400;
+        yaxes = [firstRow, secondRow, thirdRow, firstRow, thirdRow, firstRow, firstRow];
+    }
+    
+    
+    let xmax = -101;
+    let xmin = xmax * treshold;
+
+    let xaxes = 0;
+    let speed = 0;    
+    for (let i = 0; i < noOfBugs; i++) {
+        xaxes = Math.floor(Math.random() * (xmax - xmin) + xmin);
+        speed = Math.floor(Math.random() * (speedMax - speedMin) + speedMin);
+        allEnemies[i] = new Enemy(xaxes, yaxes[i] - player.centerYdiff, speed);
+    }  
+}
+
+
+/* This function does nothing but it could have been a good place to
+    * handle game reset states - maybe a new game menu or a game over screen
+    * those sorts of things. It's only called once by the init() method.
+    */
+function resetGame() {
+    lastTime = Date.now();
+    gameSetUp();       
+}
+
+// The function winModal() is to display the Modal when you win the Game
+function winModal() {    
+    let modal = document.querySelector("#modal");
+    let modalOverlay = document.querySelector("#modal-overlay");
+    let modalContent = document.querySelector("#modal-content");
+
+    stopTimer();
+    gameStarted = false;
+
+    // The DOM classList() "remove()"" function is to remove the "close" class
+    // which is defined in the main.css file, "opened" class will be
+    // added to the "modal" and the "modal-overlay" span class.
+    // It actually acts as a toggle method.
+
+    modal.classList.remove("closed");
+    modalOverlay.classList.remove("closed");
+    modal.classList.add("opened");
+    modalOverlay.classList.add("opened");
+    
+    const requiredTime = "Time required " + (minTime < 10 ? "0" + minTime : minTime) + ":" + (secTime < 10 ? "0" + secTime : secTime);
+    //This is the Modal Content, we use the Template Literals to generate the content
+    modalContent.innerHTML = `<p class="modalHeading">Congratualations! You Won!</p><p class="p3"><button onclick="winModal();" class="modal-close-button" id="close-button">Restart</button></p>`;
+    modalContent.innerHTML = `<p class="modalHeading">Congratualations! You Won!</p><p class="p1">Level: ${levels}</p><p class="p2">${requiredTime}</p><p class="p3"><button onclick="winModal();" class="modal-close-button" id="close-button">Restart</button></p>`;
+
+    let closeButton = document.querySelector("#close-button");
+
+
+    closeButton.addEventListener("click", function() {
+        modal.classList.remove("opened");
+        modalOverlay.classList.remove("opened");
+        modal.classList.add("closed");
+        modalOverlay.classList.add("closed");
+        restartGame();
+    });
+}
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    let allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
